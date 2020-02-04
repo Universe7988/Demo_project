3357c9bf3a9722eedc21e6af534f2a2591015793
Universe7988 <60619475+Universe7988@users.noreply.github.com>
2020-02-04 12:22:59 +0530

    Initial commit

---
+++ src/Concepts.java
@@ -0,0 +1,9 @@
+
+public class Concepts {
+
+	public static void main(String[] args) {
+		System.out.println("Test  Demo");
+
+	}
+
+}
---
+++ src/StaticBlockExample.java
@@ -0,0 +1,17 @@
+
+public class StaticBlockExample {
+	
+	public static String name;
+	
+	
+	static {
+		name = "Priya";
+		System.out.println(name);
+		System.exit(0);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
